0efc0647d060d907de07228c60540841ae0bce14
lcarapaica <lmcarapaica@gmail.com>
2026-05-12 14:12:39 -0400

    Bugfix: Fixed USER_ROLE being added to users

--- api_intranet/src/Entity/User.php
+++ api_intranet/src/Entity/User.php
@@ -124,8 +124,10 @@ class User implements UserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        // guarantee every user at least has ROLE_USER if no other roles are assigned
+        if (empty($roles)) {
+            $roles[] = 'ROLE_USER';
+        }
 
         return array_unique($roles);
     }
